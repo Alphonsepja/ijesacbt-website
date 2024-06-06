49b8b57b2aeb65df06cec97d300cc5efa67f7b5d
DaveToCrackCode <akave7@gmail.com>
2024-06-06 15:59:27 +0530

    comity

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,8 @@ app.use(
   cors({
     origin: "*",
   })
-); // Allow CORS for all origins
+);
+ // Allow CORS for all origins
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Corrected the method name to express.urlencoded
